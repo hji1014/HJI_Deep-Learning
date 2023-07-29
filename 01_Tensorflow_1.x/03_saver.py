eb11b0aac72a0240d83dbbe5bcc7a91c99dd4664
Junil Huh <hji10141@naver.com>
2023-07-29 09:29:40 +0900

    Add files via upload

---
+++ 01_Tensorflow_1.x/03_saver.py
@@ -0,0 +1,68 @@
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import numpy as np
+
+data = np.loadtxt('C:/Users/User/PycharmProjects/py_365/04_Data/data.csv', delimiter=',', unpack=True, dtype='float32')
+
+# 털, 날개, 기타, 포유류, 조류
+# x_data = 0, 1
+# y_data = 2, 3, 4
+x_data = np.transpose(data[0:2, :])
+y_data = np.transpose(data[2:, :])
+
+###################
+# 신경망 모델 구성
+###################
+global_step = tf.Variable(0, trainable=False, name='global_step')
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+W1 = tf.Variable(tf.random_uniform([2, 10], -1., 1.))
+L1 = tf.nn.relu(tf.matmul(X, W1))
+
+W2 = tf.Variable(tf.random_uniform([10, 20], -1., 1.))
+L2 = tf.nn.relu(tf.matmul(L1, W2))
+
+W3 = tf.Variable(tf.random_uniform([20, 3], -1., 1.))
+model = tf.matmul(L2, W3)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y, logits=model))   # global_step로 넘겨준 변수를, 학습용 변수들을 최적화 할 때 마다 학습 횟수를 하나씩 증가시킵니다.
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+train_op = optimizer.minimize(cost, global_step=global_step)
+
+###################
+# 신경망 모델 학습
+###################
+sess = tf.Session()
+
+saver = tf.train.Saver(tf.global_variables()) # 모델을 저장하고 불러오는 API를 초기화, global_variables 함수를 통해 앞서 정의하였던 변수들을 저장하거나 불러올 변수들로 설정
+
+ckpt = tf.train.get_checkpoint_state('C:/Users/User/PycharmProjects/py_365/01_Tensorflow_1/model')
+if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+    saver.restore(sess, ckpt.model_checkpoint_path)
+else:
+    sess.run(tf.global_variables_initializer())
+
+# 최적화 진행
+for step in range(2):
+    sess.run(train_op, feed_dict={X: x_data, Y: y_data})
+
+    print('Step: %d, ' % sess.run(global_step),
+          'Cost: %.3f' % sess.run(cost, feed_dict={X: x_data, Y: y_data}))
+
+# 최적화가 끝난 뒤, 변수를 저장
+saver.save(sess, 'C:/Users/User/PycharmProjects/py_365/01_Tensorflow_1/model/dnn.ckpt', global_step=global_step)
+
+#########
+# 결과 확인
+# 0: 기타 1: 포유류, 2: 조류
+######
+prediction = tf.argmax(model, 1)
+target = tf.argmax(Y, 1)
+print('예측값:', sess.run(prediction, feed_dict={X: x_data}))
+print('실제값:', sess.run(target, feed_dict={Y: y_data}))
+
+is_correct = tf.equal(prediction, target)
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+print('정확도: %.2f' % sess.run(accuracy * 100, feed_dict={X: x_data, Y: y_data}))
