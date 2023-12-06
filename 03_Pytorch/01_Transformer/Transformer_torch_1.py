471aca7f1fbd8ae8ca024027b54b86366bbc75d8
Junil Huh <hji10141@naver.com>
2023-12-06 16:36:26 +0900

    Update Transformer_torch_1.py

--- 03_Pytorch/01_Transformer/Transformer_torch_1.py
+++ 03_Pytorch/01_Transformer/Transformer_torch_1.py
@@ -1,6 +1,7 @@
 """
 [Transformer 이해하기]
-- ref : https://wikidocs.net/156986
+- ref : https://wikidocs.net/156986,
+        https://towardsdatascience.com/a-detailed-guide-to-pytorchs-nn-transformer-module-c80afbc9ffb1
 """
 import torch
 print(torch.__version__)
@@ -28,3 +29,326 @@ src = torch.rand((32, 10, 512))         # if batch_first=False -> (S, N, E) / if
 tgt = torch.rand((32, 20, 512))         # if batch_first=False -> (T, N, E) / if batch_first=True -> (N, T, E)
 out = transformer_model(src, tgt)       # if batch_first=False -> (T, N, E) / if batch_first=True -> (N, T, E)
 
+
+"""
+- nn.Transformer는 전체 구조에서 Multi-Head Attention, Feed Forward 등을 처리함
+- 즉 Linear, Positional Encoding, Embedding 등은 따로 만들어줘야 함
+- 트랜스포머 모듈은 "Attention is all you need" 모델에서 사용한 하이퍼파라미터와 함께 제공
+- Positional Encoding : 순서 정보 입력. 위치에 따라 중요도를 부여. 시퀀스의 다른 모든 요소와 관련하여 위치를 나타내는 각 요소에 대한 벡터를 생성
+    * register_buffer로 layer를 등록하면 optimizer가 업데이트하지 않고 하나의 layer로써 작용. state_dict()로 확인이 가능하고, GPU연산이 가능
+- Masking : 텍스트를 학습할 때 현재 시퀀스 이후의 단어들을 학습해서는 안됨. 정사각행렬을 사용하여 현재 시퀀스 이후의 단어 위치에 -무한대로 마스킹을 해야 함,
+            또한 토큰 정보(sequence인지 padding인지)를 모델에 알리기 위해 False(sequence), True(padding)로 구성된 정사각행렬 마스크를 만들어야 함
+"""
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import math
+import numpy as np
+import pandas as pd
+import random
+import re
+
+from torch.utils.data import DataLoader, Dataset
+
+import matplotlib.pyplot as plt
+
+# class Transformer(nn.Module):
+#     def __init__(self, num_tokens, dim_model, num_heads, num_encoder_layers, num_decoder_layers, dropout_p, ):
+#         super().__init__()
+#
+#         # Layers
+#         self.transformer = nn.Transformer(
+#             d_model=dim_model,
+#             nhead=num_heads,
+#             num_encoder_layers=num_encoder_layers,
+#             num_decoder_layers=num_decoder_layers,
+#             dropout=dropout_p,
+#         )
+#
+#     def forward(self):
+#         pass
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim_model, dropout_p, max_len):
+        super().__init__()
+        # 드롭 아웃
+        self.dropout = nn.Dropout(dropout_p)
+
+        # Encoding - From formula
+        pos_encoding = torch.zeros(max_len, dim_model)
+        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1)                         # 0, 1, 2, 3, 4, 5
+        division_term = torch.exp(torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model)     # 1000^(2i/dim_model)
+
+        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)       # a:b:c = 시작점:끝점:stride
+        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
+
+        # Saving buffer (same as parameter without gradients needed)
+        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pos_encoding", pos_encoding)      # optimizer가 업데이트 하지 않고, 하나으 layer로 작용, state_dict()로 확인 가능, GPU 연산 가능
+
+    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
+        # Residual connection + pos encoding
+        return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
+
+
+class Transformer(nn.Module):
+    # Constructor
+    def __init__(self, num_tokens, dim_model, num_heads, num_encoder_layers, num_decoder_layers, dropout_p):
+        super().__init__()
+
+        # INFO
+        self.model_type = "Transformer"
+        self.dim_model = dim_model
+
+        # LAYERS
+        self.positional_encoder = PositionalEncoding(dim_model=dim_model, dropout_p=dropout_p, max_len=5000)
+        self.embedding = nn.Embedding(num_tokens, dim_model)
+        self.transformer = nn.Transformer(
+            d_model=dim_model,
+            nhead=num_heads,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dropout=dropout_p,
+        )
+        self.out = nn.Linear(dim_model, num_tokens)
+
+    def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
+        # Src, Tgt size 는 반드시 (batch_size, src sequence length) 여야 합니다.
+
+        # Embedding + positional encoding - Out size = (batch_size, sequence length, dim_model)
+        src = self.embedding(src) * math.sqrt(self.dim_model)
+        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
+        src = self.positional_encoder(src)
+        tgt = self.positional_encoder(tgt)
+
+        src = src.permute(1, 0, 2)      # shape를 TR 입력에 맞게 (S, N, E)로 맞춤
+        tgt = tgt.permute(1, 0, 2)      # shape를 TR 입력에 맞게 (T, N, E)로 맞춤
+
+        # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
+        transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=tgt_pad_mask)
+        out = self.out(transformer_out)
+
+        return out
+
+    def get_tgt_mask(self, size) -> torch.tensor:
+        mask = torch.tril(torch.ones(size, size) == 1)          # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf'))       # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0))          # Convert ones to 0
+
+        return mask
+
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+        return (matrix == pad_token)
+
+
+def generate_random_data(n):
+    SOS_token = np.array([2])
+    EOS_token = np.array([3])
+    length = 8
+
+    data = []
+
+    # 1,1,1,1,1,1 -> 1,1,1,1,1
+    for i in range(n // 3):
+        X = np.concatenate((SOS_token, np.ones(length), EOS_token))
+        y = np.concatenate((SOS_token, np.ones(length), EOS_token))
+        data.append([X, y])
+
+    # 0,0,0,0 -> 0,0,0,0
+    for i in range(n // 3):
+        X = np.concatenate((SOS_token, np.zeros(length), EOS_token))
+        y = np.concatenate((SOS_token, np.zeros(length), EOS_token))
+        data.append([X, y])
+
+    # 1,0,1,0 -> 1,0,1,0,1
+    for i in range(n // 3):
+        X = np.zeros(length)
+        start = random.randint(0, 1)
+
+        X[start::2] = 1
+
+        y = np.zeros(length)
+        if X[-1] == 0:
+            y[::2] = 1
+        else:
+            y[1::2] = 1
+
+        X = np.concatenate((SOS_token, X, EOS_token))
+        y = np.concatenate((SOS_token, y, EOS_token))
+        data.append([X, y])
+
+    np.random.shuffle(data)
+
+    return data
+
+
+#크기가 16인 배치 형태로 만들어 줍니다.
+def batchify_data(data, batch_size=16, padding=False, padding_token=-1):
+    batches = []
+    for idx in range(0, len(data), batch_size):
+        # batch_size 크기가 아닌 경우 마지막 비트를 얻지 않도록 합니다.
+        if idx + batch_size < len(data):
+            # 여기서 배치의 최대 길이를 가져와 PAD 토큰으로 길이를 정규화해야 합니다.
+            if padding:
+                max_batch_length = 0
+                # batch에서 가장 긴 문장 가져오기
+                for seq in data[idx : idx + batch_size]:
+                    if len(seq) > max_batch_length:
+                        max_batch_length = len(seq)
+
+                # 최대 길이에 도달할 때까지 X 패딩 토큰을 추가합니다.
+                for seq_idx in range(batch_size):
+                    remaining_length = max_bath_length - len(data[idx + seq_idx])
+                    data[idx + seq_idx] += [padding_token] * remaining_length
+
+            batches.append(np.array(data[idx : idx + batch_size]).astype(np.int64))
+
+    print(f"{len(batches)} batches of size {batch_size}")
+
+    return batches
+
+
+train_data = generate_random_data(9000)
+val_data = generate_random_data(3000)
+
+train_dataloader = batchify_data(train_data)
+val_dataloader = batchify_data(val_data)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = Transformer(num_tokens=4, dim_model=8, num_heads=2, num_encoder_layers=3, num_decoder_layers=3, dropout_p=0.1).to(device)
+opt = torch.optim.SGD(model.parameters(), lr=0.01)
+loss_fn = nn.CrossEntropyLoss()
+
+
+def train_loop(model, opt, loss_fn, dataloader):
+    model.train()
+    total_loss = 0
+
+    for batch in dataloader:
+        X, y = batch[:, 0], batch[:, 1]
+        X, y = torch.tensor(X).to(device), torch.tensor(y).to(device)
+
+        # 이제 tgt를 1만큼 이동하여 <SOS>를 사용하여 pos 1에서 토큰을 예측합니다.
+        y_input = y[:, :-1]
+        y_expected = y[:, 1:]
+
+        # 다음 단어를 마스킹하려면 마스크 가져오기
+        sequence_length = y_input.size(1)
+        tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+
+        # X, y_input 및 tgt_mask를 전달하여 표준 training
+        pred = model(X, y_input, tgt_mask)
+
+        # Permute 를 수행하여 batch size 가 처음이 되도록
+        pred = pred.permute(1, 2, 0)
+        loss = loss_fn(pred, y_expected)
+
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        total_loss += loss.detach().item()
+
+    return total_loss / len(dataloader)
+
+
+def validation_loop(model, loss_fn, dataloader):
+    model.eval()
+    total_loss = 0
+
+    with torch.no_grad():
+        for batch in dataloader:
+            X, y = batch[:, 0], batch[:, 1]
+            X, y = torch.tensor(X, dtype=torch.long, device=device), torch.tensor(y, dtype=torch.long, device=device)
+
+            y_input = y[:,:-1]
+            y_expected = y[:,1:]
+
+            sequence_length = y_input.size(1)
+            tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+
+            pred = model(X, y_input, tgt_mask)
+
+            pred = pred.permute(1, 2, 0)
+            loss = loss_fn(pred, y_expected)
+            total_loss += loss.detach().item()
+
+    return total_loss / len(dataloader)
+
+
+def fit(model, opt, loss_fn, train_dataloader, val_dataloader, epochs):
+    # 나중에 plotting 하기위해
+    train_loss_list, validation_loss_list = [], []
+
+    print("Training and validating model")
+    for epoch in range(epochs):
+        print("-"*25, f"Epoch {epoch + 1}","-"*25)
+
+        train_loss = train_loop(model, opt, loss_fn, train_dataloader)
+        train_loss_list += [train_loss]
+
+        validation_loss = validation_loop(model, loss_fn, val_dataloader)
+        validation_loss_list += [validation_loss]
+
+        print(f"Training loss: {train_loss:.4f}")
+        print(f"Validation loss: {validation_loss:.4f}")
+        print()
+
+    return train_loss_list, validation_loss_list
+
+train_loss_list, validation_loss_list = fit(model, opt, loss_fn, train_dataloader, val_dataloader, 10)
+
+plt.plot(train_loss_list, label="Train loss")
+plt.plot(validation_loss_list, label="Validation loss")
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Loss vs Epoch')
+plt.legend()
+plt.show()
+
+
+def predict(model, input_sequence, max_length=15, SOS_token=2, EOS_token=3):
+    model.eval()
+
+    y_input = torch.tensor([[SOS_token]], dtype=torch.long, device=device)
+
+    num_tokens = len(input_sequence[0])
+
+    for _ in range(max_length):
+        # Get source mask
+        tgt_mask = model.get_tgt_mask(y_input.size(1)).to(device)
+
+        pred = model(input_sequence, y_input, tgt_mask)
+
+        next_item = pred.topk(1)[1].view(-1)[-1].item() # num with highest probability
+        next_item = torch.tensor([[next_item]], device=device)
+
+        # Concatenate previous input with predicted best word
+        y_input = torch.cat((y_input, next_item), dim=1)
+
+        # Stop if model predicts end of sentence
+        if next_item.view(-1).item() == EOS_token:
+            break
+
+    return y_input.view(-1).tolist()
+
+
+# Here we test some examples to observe how the model predicts
+examples = [
+    torch.tensor([[2, 0, 0, 0, 0, 0, 0, 0, 0, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 1, 1, 1, 1, 1, 1, 1, 1, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 1, 0, 1, 0, 1, 0, 1, 0, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 0, 1, 0, 1, 0, 1, 0, 1, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 3]], dtype=torch.long, device=device),
+    torch.tensor([[2, 0, 1, 3]], dtype=torch.long, device=device)
+]
+
+for idx, example in enumerate(examples):
+    result = predict(model, example)
+    print(f"Example {idx}")
+    print(f"Input: {example.view(-1).tolist()[1:-1]}")
+    print(f"Continuation: {result[1:-1]}")
+    print()
