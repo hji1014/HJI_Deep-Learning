1670c0cd16daccaedfb59c05b3aff53750c16cb6
Junil Huh <hji10141@naver.com>
2023-12-27 09:38:17 +0900

    Update ClipCap_flickr8k.py

--- 03_Pytorch/06_ClipCap/ClipCap_flickr8k.py
+++ 03_Pytorch/06_ClipCap/ClipCap_flickr8k.py
@@ -436,3 +436,119 @@ train(dataset,
       )
 
 # =================================================================================================================== #
+
+#@title Caption prediction
+
+def generate_beam(model, tokenizer, beam_size: int = 5, prompt=None, embed=None, entry_length=67, temperature=1., stop_token: str = '.'):
+    model.eval()
+    stop_token_index = tokenizer.encode(stop_token)[0]
+    tokens = None
+    scores = None
+    device = next(model.parameters()).device
+    seq_lengths = torch.ones(beam_size, device=device)
+    is_stopped = torch.zeros(beam_size, device=device, dtype=torch.bool)
+    with torch.no_grad():
+        if embed is not None:
+            generated = embed
+        else:
+            if tokens is None:
+                tokens = torch.tensor(tokenizer.encode(prompt))
+                tokens = tokens.unsqueeze(0).to(device)
+                generated = model.gpt.transformer.wte(tokens)
+        for i in range(entry_length):
+            outputs = model.gpt(inputs_embeds=generated)
+            logits = outputs.logits
+            logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+            logits = logits.softmax(-1).log()
+            if scores is None:
+                scores, next_tokens = logits.topk(beam_size, -1)
+                generated = generated.expand(beam_size, *generated.shape[1:])
+                next_tokens, scores = next_tokens.permute(1, 0), scores.squeeze(0)
+                if tokens is None:
+                    tokens = next_tokens
+                else:
+                    tokens = tokens.expand(beam_size, *tokens.shape[1:])
+                    tokens = torch.cat((tokens, next_tokens), dim=1)
+            else:
+                logits[is_stopped] = -float(np.inf)
+                logits[is_stopped, 0] = 0
+                scores_sum = scores[:, None] + logits
+                seq_lengths[~is_stopped] += 1
+                scores_sum_average = scores_sum / seq_lengths[:, None]
+                scores_sum_average, next_tokens = scores_sum_average.view(-1).topk(beam_size, -1)
+                next_tokens_source = next_tokens // scores_sum.shape[1]
+                seq_lengths = seq_lengths[next_tokens_source]
+                next_tokens = next_tokens % scores_sum.shape[1]
+                next_tokens = next_tokens.unsqueeze(1)
+                tokens = tokens[next_tokens_source]
+                tokens = torch.cat((tokens, next_tokens), dim=1)
+                generated = generated[next_tokens_source]
+                scores = scores_sum_average * seq_lengths
+                is_stopped = is_stopped[next_tokens_source]
+            next_token_embed = model.gpt.transformer.wte(next_tokens.squeeze()).view(generated.shape[0], 1, -1)
+            generated = torch.cat((generated, next_token_embed), dim=1)
+            is_stopped = is_stopped + next_tokens.eq(stop_token_index).squeeze()
+            if is_stopped.all():
+                break
+    scores = scores / seq_lengths
+    output_list = tokens.cpu().numpy()
+    output_texts = [tokenizer.decode(output[:int(length)]) for output, length in zip(output_list, seq_lengths)]
+    order = scores.argsort(descending=True)
+    output_texts = [output_texts[i] for i in order]
+    return output_texts
+
+# =================================================================================================================== #
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
+
+clip_model, preprocess = clip.load("RN50x4", device=device, jit=False)
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+# =================================================================================================================== #
+
+prefix_length = 40
+
+model = ClipCaptionPrefix(prefix_length,
+                          clip_length=40,
+                          prefix_size=640,
+                          num_layers=8,
+                          mapping_type='transformer'
+                          )
+model.load_state_dict(torch.load('./coco_prefix-009.pt', map_location='cpu'))
+model = model.eval()
+model = model.to(device)
+
+# =================================================================================================================== #
+
+images = Image.open('C:/Users/user02/Desktop/허준일/3. 국립암센터/0. 딥러닝 총정리/딥러닝 결과 정리/20231227_ClipCap_1_bs64_ep10/KakaoTalk_20231227_092947581.jpg').convert("RGB")
+image = preprocess(images).unsqueeze(0).to(device)
+plt.imshow(images)
+plt.show()
+
+# =================================================================================================================== #
+
+use_beam_search = True
+
+with torch.no_grad():
+    prefix = clip_model.encode_image(image).to(device, dtype=torch.float32)
+    prefix = prefix / prefix.norm(2, -1).item()
+    prefix_embed = model.clip_project(prefix).reshape(1, prefix_length, -1)
+if use_beam_search:
+    generated_text_prefix = generate_beam(model,
+                                          tokenizer,
+                                          beam_size=7,
+                                          embed=prefix_embed,
+                                          stop_token='<|endoftext|>',
+                                          entry_length=30
+                                          )[0]
+
+print('\n')
+print(generated_text_prefix)
+
+# =================================================================================================================== #
