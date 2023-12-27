23242d8a3cb137684bdec01706b081245e258635
Junil Huh <hji10141@naver.com>
2023-12-27 15:43:11 +0900

    Update ClipCap_flickr8k.py

--- 03_Pytorch/06_ClipCap/ClipCap_flickr8k.py
+++ 03_Pytorch/06_ClipCap/ClipCap_flickr8k.py
@@ -427,7 +427,7 @@ model = ClipCaptionPrefix(40,
 train(dataset,
       model,
       bs=64,
-      epochs=10,        # 원래 20
+      epochs=20,        # 원래 20
       save_every=5,
       lr=2e-5,
       warmup_steps=5000,
@@ -439,7 +439,16 @@ train(dataset,
 
 #@title Caption prediction
 
-def generate_beam(model, tokenizer, beam_size: int = 5, prompt=None, embed=None, entry_length=67, temperature=1., stop_token: str = '.'):
+def generate_beam(
+        model,
+        tokenizer,
+        beam_size: int = 5,
+        prompt=None,
+        embed=None,
+        entry_length=67,
+        temperature=1.,
+        stop_token: str = '.'
+):
     model.eval()
     stop_token_index = tokenizer.encode(stop_token)[0]
     tokens = None
@@ -497,6 +506,68 @@ def generate_beam(model, tokenizer, beam_size: int = 5, prompt=None, embed=None,
     output_texts = [output_texts[i] for i in order]
     return output_texts
 
+def generate2(
+        model,
+        tokenizer,
+        tokens=None,
+        prompt=None,
+        embed=None,
+        entry_count=1,
+        entry_length=67,  # maximum number of words
+        top_p=0.8,
+        temperature=1.,
+        stop_token: str = '.',
+):
+    model.eval()
+    generated_num = 0
+    generated_list = []
+    stop_token_index = tokenizer.encode(stop_token)[0]
+    filter_value = -float("Inf")
+    device = next(model.parameters()).device
+
+    with torch.no_grad():
+
+        for entry_idx in trange(entry_count):
+            if embed is not None:
+                generated = embed
+            else:
+                if tokens is None:
+                    tokens = torch.tensor(tokenizer.encode(prompt))
+                    tokens = tokens.unsqueeze(0).to(device)
+
+                generated = model.gpt.transformer.wte(tokens)
+
+            for i in range(entry_length):
+
+                outputs = model.gpt(inputs_embeds=generated)
+                logits = outputs.logits
+                logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+                sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+                cumulative_probs = torch.cumsum(nnf.softmax(sorted_logits, dim=-1), dim=-1)
+                sorted_indices_to_remove = cumulative_probs > top_p
+                sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+                                                    ..., :-1
+                                                    ].clone()
+                sorted_indices_to_remove[..., 0] = 0
+
+                indices_to_remove = sorted_indices[sorted_indices_to_remove]
+                logits[:, indices_to_remove] = filter_value
+                next_token = torch.argmax(logits, -1).unsqueeze(0)
+                next_token_embed = model.gpt.transformer.wte(next_token)
+                if tokens is None:
+                    tokens = next_token
+                else:
+                    tokens = torch.cat((tokens, next_token), dim=1)
+                generated = torch.cat((generated, next_token_embed), dim=1)
+                if stop_token_index == next_token.item():
+                    break
+
+            output_list = list(tokens.squeeze().cpu().numpy())
+            output_text = tokenizer.decode(output_list)
+            generated_list.append(output_text)
+
+    return generated_list[0]
+
 # =================================================================================================================== #
 
 if torch.cuda.is_available():
@@ -526,14 +597,14 @@ model = model.to(device)
 
 # =================================================================================================================== #
 
-images = Image.open('C:/Users/user02/Desktop/허준일/3. 국립암센터/0. 딥러닝 총정리/딥러닝 결과 정리/20231227_ClipCap_1_bs64_ep10/KakaoTalk_20231227_092947581.jpg').convert("RGB")
+images = Image.open('C:/Users/user02/Desktop/허준일/3. 국립암센터/0. 딥러닝 총정리/딥러닝 결과 정리/20231227_ClipCap_1_bs64_ep10/KakaoTalk_20231227_093326849.jpg').convert("RGB")
 image = preprocess(images).unsqueeze(0).to(device)
 plt.imshow(images)
 plt.show()
 
 # =================================================================================================================== #
 
-use_beam_search = True
+use_beam_search = False     # beam_search 사용할지 말지 결정
 
 with torch.no_grad():
     prefix = clip_model.encode_image(image).to(device, dtype=torch.float32)
@@ -546,9 +617,11 @@ if use_beam_search:
                                           embed=prefix_embed,
                                           stop_token='<|endoftext|>',
                                           entry_length=30
-                                          )[0]
-
-print('\n')
-print(generated_text_prefix)
-
-# =================================================================================================================== #
+                                          )
+    #generated_text_prefix = generate_beam(model, tokenizer, embed=prefix_embed)[0]
+    print('\n')
+    print(generated_text_prefix[0])
+else:
+    generated_text_prefix = generate2(model, tokenizer, embed=prefix_embed)
+    print('\n')
+    print(generated_text_prefix)
